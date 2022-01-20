7a47f3d621786bce06524d555092e79eaafd8c3e
Joey Lim <jyorin0206@gmail.com>
2022-01-20 13:37:44 +0800

    Added UserServlet.java imports and constants

---
+++ DevopS/src/main/java/UserServlet.java
@@ -0,0 +1,60 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.RequestDispatcher;
+
+/**
+ * Servlet implementation class UserServlet
+ */
+@WebServlet("/UserServlet")
+public class UserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	//Step 1: Prepare list of variables used for database connections
+	private String jdbcURL = "jdbc:mysql://localhost:3306/userdetails";
+	private String jdbcUsername = "root";
+	private String jdbcPassword = "password";
+	//Step 2: Prepare list of SQL prepared statements to perform CRUD to our database
+	private static final String INSERT_USERS_SQL = "INSERT INTO UserDetails" + " (name, password, email, language) VALUES " +
+	" (?, ?, ?);";
+	private static final String SELECT_USER_BY_ID = "select name,password,email,language from UserDetails where name =?";
+	private static final String SELECT_ALL_USERS = "select * from UserDetails ";
+	private static final String DELETE_USERS_SQL = "delete from UserDetails where name = ?;";
+	private static final String UPDATE_USERS_SQL = "update UserDetails set name = ?,password= ?, email =?,language =? where name = ?;";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
